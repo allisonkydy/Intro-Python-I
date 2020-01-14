4b01c8ffdb29dce7f8ef97a2783666fd01896b0f
allisonkydy <allisnelly@gmail.com>
2020-01-14 14:23:06 -0600

    refactor 14

--- src/14_cal.py
+++ src/14_cal.py
@@ -23,15 +23,17 @@ import sys
 import calendar
 from datetime import datetime
 
-user_input = sys.argv
-my_cal = calendar.TextCalendar(firstweekday=6)
-date_today = datetime.today()
 
-if len(user_input) == 1:
+my_cal = calendar.TextCalendar(firstweekday=6)
+date_today = datetime.now()
+
+if len(sys.argv) == 1:
     my_cal.prmonth(date_today.year, date_today.month)
-elif len(user_input) == 2:
-    my_cal.prmonth(date_today.year, int(user_input[1]))
-elif len(user_input) == 3:
-    my_cal.prmonth(int(user_input[2]), int(user_input[1]))
+elif len(sys.argv) == 2:
+    my_cal.prmonth(date_today.year, int(sys.argv[1]))
+elif len(sys.argv) == 3:
+    my_cal.prmonth(int(sys.argv[2]), int(sys.argv[1]))
 else:
     print("Usage: 14_cal.py month [year]")
+
+sys.exit()
